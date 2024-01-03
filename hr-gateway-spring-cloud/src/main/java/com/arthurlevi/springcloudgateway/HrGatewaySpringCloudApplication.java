4def882841a9f6e5f3ae9769c953171ee048d856
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-01-03 11:53:33 -0300

    hr-gateway-spring-cloud created

---
+++ hr-gateway-spring-cloud/src/main/java/com/arthurlevi/springcloudgateway/HrGatewaySpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.arthurlevi.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGatewaySpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrGatewaySpringCloudApplication.class, args);
+	}
+
+}
---
+++ hr-gateway-spring-cloud/src/test/java/com/arthurlevi/springcloudgateway/HrGatewaySpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurlevi.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewaySpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
