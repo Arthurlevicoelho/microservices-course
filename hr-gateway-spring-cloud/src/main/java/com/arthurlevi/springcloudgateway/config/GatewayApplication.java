e5236c77440434e95ed1e2d3cb9311649c045929
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-01-03 15:17:03 -0300

    Implementing Spring Cloud Gateway

---
+++ hr-gateway-spring-cloud/src/main/java/com/arthurlevi/springcloudgateway/config/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.arthurlevi.springcloudgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes() 
+			    .route( "hr-worker" , r -> r.path( "/hr-worker/**" ) 
+			      .filters(f -> f.stripPrefix( 1 )) 
+			      .uri( "lb://HR-WORKER" )) 
+			    .route( "hr-payroll" , r -> r.path( "/hr-payroll/**" ) 
+			      .filters(f -> f.stripPrefix( 1 )) 
+			      .uri( "lb://HR-PAYROLL" )) 
+			    .build(); 
+			  } 
+
+}
