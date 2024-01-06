b65cfa01e9e216026e5fd76df400874c53dab16e
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-01-06 13:36:33 -0300

    hr-user created

---
+++ hr-user/src/test/java/com/arthurlevi/user/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurlevi.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
